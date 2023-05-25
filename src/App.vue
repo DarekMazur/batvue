71fe9d823adaf2b9ec903802c90ce5323fa7f40e
Darek Mazur <dev@darekmazur.it>
2023-05-25 22:13:40 +0200

    create main view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <p>Hello World!</p>
+  <div class="App">
+    <header>BatQuiz</header>
+    <main>
+      <section>
+        <h3>Lorem ipsum</h3>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At,
+            eveniet, porro? Doloremque ducimus facere nisi. Esse est labore
+            laudantium magnam magni natus odit quam repudiandae similique,
+            soluta ut veniam voluptate!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium ad, asperiores commodi consequuntur deleniti dicta
+            doloremque dolores earum eligendi id ipsa iure laborum maiores nemo
+            odit perspiciatis, quisquam quod repellendus!
+          </p>
+        </div>
+        <div class="formWrapper">
+          <button>Easy</button>
+          <button>Normal</button>
+          <button>Hard</button>
+          <input />
+        </div>
+        <div class="navigation">
+          <button class="resetButton">Reset</button>
+          <button class="submitButton">Start</button>
+        </div>
+      </section>
+    </main>
+    <footer>Footer content</footer>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
